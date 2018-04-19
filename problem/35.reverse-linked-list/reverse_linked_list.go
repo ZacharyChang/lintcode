d6d54652573013bcae628e3b185763c076f4fabe
zachary <zhangzhipeng@mail.sdmc.com>
2018-04-19 20:31:53 +0800

    update: update #35

--- problem/35.reverse-linked-list/reverse_linked_list.go
+++ problem/35.reverse-linked-list/reverse_linked_list.go
@@ -9,6 +9,8 @@ type ListNode struct {
 }
 
 /**
+ * 数组暂存ListNode，然后改变Next指向
+ * 需要额外消耗O(n)的空间
  * @param head: n
  * @return: The new head of reversed linked list.
  */
@@ -30,4 +32,24 @@ func reverse(head *ListNode) *ListNode {
 	}
 	array[0].Next = nil
 	return array[len(array)-1]
+}
+
+/**
+ * 递归方法，不断递归直到head指向最后节点
+ * 每次调转head与下一届点顺序，直到递归结束
+ * 新链表的头在递归过程中不改变，依次向上返回
+ * @param head: n
+ * @return: The new head of reversed linked list.
+ */
+func solution2(head *ListNode) *ListNode {
+	if head == nil {
+		return nil
+	}
+	if head.Next == nil {
+		return head
+	}
+	newHead := solution2(head.Next)
+	head.Next.Next = head
+	head.Next = nil
+	return newHead
 }
