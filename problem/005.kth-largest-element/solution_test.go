3f5e28eef726d39343086ca24e35fa850ecaf6d7
Zachary <zacharychang.zc@gmail.com>
2018-05-16 21:23:41 +0800

    fix: update test cases

--- problem/005.kth-largest-element/solution_test.go
+++ problem/005.kth-largest-element/solution_test.go
@@ -2,10 +2,10 @@ package lintcode
 
 import "testing"
 
-func Test_findKthMax(t *testing.T) {
+func Test_kthLargestElement(t *testing.T) {
 	type args struct {
+		n    int
 		nums []int
-		k    int
 	}
 	tests := []struct {
 		name string
@@ -15,40 +15,40 @@ func Test_findKthMax(t *testing.T) {
 		{
 			"[Test Case 1]",
 			args{
-				[]int{5, 4, 3, 2, 1},
 				1,
+				[]int{5, 4, 3, 2, 1},
 			},
 			5,
 		},
 		{
 			"[Test Case 2]",
 			args{
-				[]int{6, 5, 2, 3, 4, 1},
 				2,
+				[]int{6, 5, 2, 3, 4, 1},
 			},
 			5,
 		},
 		{
 			"[Test Case 3]",
 			args{
-				[]int{3, 3, 3, 2, 2, 1, 6, 5, 8, 2, 2},
 				1,
+				[]int{3, 3, 3, 2, 2, 1, 6, 5, 8, 2, 2},
 			},
 			8,
 		},
 		{
 			"[Test Case 4]",
 			args{
-				[]int{6, 5, 8, 6, 6},
 				5,
+				[]int{6, 5, 8, 6, 6},
 			},
 			5,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := findKthMax(tt.args.nums, tt.args.k); got != tt.want {
-				t.Errorf("findKthMax() = %v, want %v", got, tt.want)
+			if got := kthLargestElement(tt.args.n, tt.args.nums); got != tt.want {
+				t.Errorf("kthLargestElement() = %v, want %v", got, tt.want)
 			}
 		})
 	}
